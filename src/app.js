7cb881bd7687a135721b92321589524671b31efe
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-07-05 23:44:27 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var who = ["my dog", "my sister", "yoda"];
+var how = ["ate", "swallowed", "stole"];
+var what = ["my homework", "the laptop", " the computer"];
+var when = ["yesterday", "last night", "today"];
+var where = ["my house", "the pool", "the garage"];
+
+function generateExcuse() {
+  let randomwho = Math.floor(Math.random() * who.length);
+  let randomhow = Math.floor(Math.random() * how.length);
+  let randomwhat = Math.floor(Math.random() * what.length);
+  let randomwhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomwho] +
+    " " +
+    how[randomhow] +
+    " " +
+    what[randomwhat] +
+    " " +
+    when[randomwhen] +
+    "."
+  );
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excuse;
 };
